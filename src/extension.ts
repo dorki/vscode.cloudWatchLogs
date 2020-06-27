402745a40effb37dd526db4ac83085cda68bb146
Doron David <doron@ermetic.com>
2020-06-27 13:20:18 +0300

    commit

---
+++ src/extension.ts
@@ -0,0 +1,221 @@
+import * as vscode from 'vscode';
+import * as AWS from 'aws-sdk';
+import parseDuration from 'parse-duration';
+import { execSync } from 'child_process';
+import { BuildQueryResultsHtml, BuildLogRecordHtml } from './htmlHelper';
+import * as _ from 'lodash';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let envToCredentials: { [id: string]: AWS.Credentials } = {};
+
+	async function getEnvCredentials(env: string) {
+
+		function validateCredentials(credentials: AWS.Credentials) {
+			try {
+				new AWS.STS({ credentials }).getCallerIdentity();
+				return true;
+			}
+			catch {
+				return false;
+			}
+		}
+
+		let credentials = envToCredentials[env];
+		if (credentials != undefined && validateCredentials(credentials)) {
+			return credentials;
+		}
+
+		const authenticationCommand = vscode.workspace.getConfiguration('cloudq').get("authenticationCommand") as string;
+		if (!_.isEmpty(authenticationCommand)) {
+			execSync(authenticationCommand.replace('{env}', env));
+		}
+
+		credentials = new AWS.SharedIniFileCredentials({ profile: env });
+		if (validateCredentials(credentials)) {
+			envToCredentials[env] = credentials;
+			return credentials;
+		}
+
+		credentials = new AWS.EnvironmentCredentials("AWS");
+		if (validateCredentials(credentials)) {
+			envToCredentials[env] = credentials;
+			return credentials;
+		}
+
+		throw "could not find credentials";
+	}
+
+	function getQueryText() {
+		function isEditorDocumentLineEmpty(line: number) {
+			return vscode.window.activeTextEditor!.document.lineAt(line).isEmptyOrWhitespace;
+		}
+
+		function findQueryStart(line: number) {
+			while (line !== 0 && !isEditorDocumentLineEmpty(line)) {
+				line--;
+			}
+
+			return line;
+		}
+
+		function findQueryEnd(line: number) {
+			const editorEnd = vscode.window.activeTextEditor!.document.lineCount - 1;
+			while (line !== editorEnd && !isEditorDocumentLineEmpty(line)) {
+				line++;
+			}
+
+			return line;
+		}
+
+		if (!vscode.window.activeTextEditor!.selection.isEmpty) {
+			return vscode.window.activeTextEditor!.document.getText(vscode.window.activeTextEditor?.selection)
+		}
+
+		let startLine = findQueryStart(vscode.window.activeTextEditor!.selection.start.line);
+		let endLine = findQueryEnd(vscode.window.activeTextEditor!.selection.end.line);
+
+		const queryRange =
+			new vscode.Range(
+				new vscode.Position(startLine, 0),
+				new vscode.Position(
+					endLine,
+					vscode.window.activeTextEditor!.document.lineAt(endLine).range.end.character)
+			);
+
+		return vscode.window.activeTextEditor!.document.getText(queryRange).trim();
+	}
+
+	async function GoToLog(logPtr: string, env: string, region: string) {
+		const credentials = await getEnvCredentials(env);
+		const logs = new AWS.CloudWatchLogs({ credentials, region });
+
+		const logRecordResponse = await logs.getLogRecord({ logRecordPointer: logPtr }).promise();
+		const panel =
+			vscode.window.createWebviewPanel(
+				`Log${Date.now()}`,
+				`Log`,
+				vscode.ViewColumn.Active,
+				{
+					enableFindWidget: true,
+					retainContextWhenHidden: true
+				}
+			);
+
+		panel.webview.html = BuildLogRecordHtml(logRecordResponse.logRecord!);
+	}
+
+	type Query = {
+		cwQuery: string,
+		fields: string[],
+		env: string,
+		region: string,
+		logGroup: string,
+		duration: number
+	}
+
+	function parseQuery(query: string): Query {
+		const [settings, ...queryLines] = query.trim().split("\n");
+		const cwQuery = queryLines.join("\n");
+		const fields = query.match(/fields (.+)/)![1].split(",").map(field => field.trim());
+		const [env, region, logGroup, durationStr] = settings.split(":");
+		const duration = parseDuration(durationStr)!;
+
+		return {
+			cwQuery,
+			fields,
+			env,
+			region,
+			logGroup,
+			duration
+		};
+	}
+
+	async function executeQuery(query: Query, panel: vscode.WebviewPanel) {
+
+		const creds = await getEnvCredentials(query.env);
+		const logs = new AWS.CloudWatchLogs({ credentials: creds, region: query.region });
+
+		const endTimeMs = Date.now();
+		const startTimeMS = endTimeMs - query.duration;
+		const startQueryResponse =
+			await logs.
+				startQuery({
+					startTime: startTimeMS / 1000,
+					endTime: endTimeMs / 1000,
+					queryString: query.cwQuery,
+					logGroupName: query.logGroup
+				}).
+				promise();
+
+		let queryResultsResponse: AWS.CloudWatchLogs.GetQueryResultsResponse;
+
+		await vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			cancellable: false,
+			title: 'Loading...'
+		}, async (progress) => {
+
+			progress.report({ increment: 0 });
+
+			do {
+				queryResultsResponse =
+					await logs.
+						getQueryResults({ queryId: startQueryResponse.queryId! }).
+						promise();
+
+				progress.report({ increment: 40, message: queryResultsResponse.status });
+
+				panel.webview.html =
+					BuildQueryResultsHtml(
+						context.extensionPath,
+						query.cwQuery,
+						startTimeMS,
+						endTimeMs,
+						query.fields,
+						queryResultsResponse.results!);
+			}
+			while (queryResultsResponse.status !== "Complete");
+
+			progress.report({ increment: 100 });
+		});
+	}
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'extension.cloudWatchLogs',
+			async () => {
+				const query = parseQuery(getQueryText());
+				const panel =
+					vscode.window.createWebviewPanel(
+						`Query${Date.now()}`,
+						`Results ${query.env} ${query.logGroup}`,
+						vscode.ViewColumn.Active,
+						{
+							enableFindWidget: true,
+							enableScripts: true,
+							retainContextWhenHidden: true
+						}
+					);
+
+				panel.webview.html = "loading...";
+				await executeQuery(query, panel);
+
+				panel.webview.onDidReceiveMessage(
+					async message => {
+						console.log(message);
+						vscode.window.showInformationMessage(message);
+						switch (message.command) {
+							case 'goToLog':
+								await GoToLog(message.text, query.env, query.region);
+								return;
+							case 'refresh':
+								await executeQuery(query, panel);
+								return;
+						}
+					},
+					undefined,
+					context.subscriptions
+				);
+			}));
+}
---
+++ src/htmlHelper.ts
@@ -0,0 +1,199 @@
+import * as AWS from 'aws-sdk';
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as _ from 'lodash';
+
+export function BuildLogRecordHtml(logRecord: AWS.CloudWatchLogs.LogRecord) {
+
+    function stringify(value: string) {
+        try {
+            return JSON.stringify(JSON.parse(value), null, 4);
+        }
+        catch {
+            return value;
+        }
+    }
+
+    function getRow(fieldName: string, value: string): string {
+        return (`
+            <tr>
+                <td>${fieldName}</td>
+                <td><pre>${fieldName === "@ingestionTime" ? new Date(Number(value)).toLocaleString('he') : stringify(value)}</pre></td>
+            </tr>
+        `);
+    }
+
+    return (`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <style>
+                    table {
+                        border-collapse: collapse;
+                        border-spacing: 0px;
+                    }
+                    th, td {
+                        border: 1px solid dimgrey;
+                        padding: 5px;
+                    }
+                    pre {
+                        font-family: arial;
+                        margin: 0px;
+                        line-height: 1.1rem;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Log record</h1>
+                <br>
+                <table>
+                ${Object.entries(logRecord).sort((a, b) => a.toLocaleString().toLowerCase().localeCompare(b.toLocaleString().toLowerCase())).map(logRecordField => getRow(logRecordField[0], logRecordField[1])).join("")}
+                </table>
+            </body>
+        </html>
+        `);
+}
+
+export function BuildQueryResultsHtml(
+    extensionPath: string,
+    query: string,
+    startTimeMs: number,
+    endTimeMs: number,
+    fields: string[],
+    queryResults: AWS.CloudWatchLogs.QueryResults) {
+
+    const specialColFields = {
+        "MessageTemplate": "longCol",
+        "@timestamp": "medCol"
+    };
+
+    function getTh(value: string): string {
+        return `<th>${value}<span class="resize-handle"></span></th>`;
+    }
+
+    function getTd(value: string | undefined): string {
+        return `<td>${value?.trim() ?? ""}</td>`;
+    }
+
+    function buildQueryResultHtml(queryResult: AWS.CloudWatchLogs.ResultRows): string {
+        const fieldNameToValueMap =
+            new Map(
+                queryResult.map(
+                    queryResultField => [
+                        queryResultField.field,
+                        queryResultField.value]));
+        return (
+            `<tr>
+                <td><button onclick="goToLog('${fieldNameToValueMap.get("@ptr")}')" />🔍</td>
+                ${fields.map(field => getTd(fieldNameToValueMap.get(field)))}
+            </tr>`);
+    }
+
+    function pathPartsToUri(...pathParts: string[]) {
+        return vscode.Uri.file(path.join(extensionPath, ...pathParts)).with({ scheme: 'vscode-resource' });
+    }
+
+    function getShortColIndexes() {
+        return _(fields).
+            map((field, index) => _.has(specialColFields, field) ? undefined : index + 1).
+            filter().
+            value();
+    }
+
+    function getSpecialColIndexes(specialField: string) {
+        return _(fields).
+            map((field, index) => _.get(specialColFields, field) === specialField ? index + 1 : undefined).
+            filter().
+            value();
+    }
+
+    return (`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <link rel="stylesheet" href="${pathPartsToUri('src', 'tableStyle.css')}">
+                <link rel="stylesheet" href="${pathPartsToUri('node_modules', 'datatables.net-dt', 'css', 'jquery.dataTables.min.css')}">
+                <script type="text/javascript" charset="utf8" src="${pathPartsToUri('node_modules', 'jquery', 'dist', 'jquery.min.js')}"></script>
+                <script type="text/javascript" charset="utf8" src="${pathPartsToUri('node_modules', 'datatables.net', 'js', 'jquery.dataTables.min.js')}"></script>
+                <style>
+                    pre {
+                        font-family: arial;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1><button class='refreshButton' onclick="refresh()">Refresh</button> Query results (${queryResults.length} results)</h1>
+                <h3>Time range: ${new Date(startTimeMs).toLocaleString('he')} - ${new Date(endTimeMs).toLocaleString('he')} (Local time)</h3>
+                <pre>${query}</pre>
+                <div class="tableContainer">
+                    <div class='toggler'>
+                        Toggle columns:
+                        ${fields.map((field, index) => `<a class="toggle-vis" data-column="${index + 1}">${field}</a>`).join(" - ")}
+                    </div>
+                    <table id="resultsTable" class="display compact" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                ${fields.map(getTh).join("")}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            ${queryResults.map(buildQueryResultHtml).join("")}
+                        </tbody>
+                    </table>
+                </div>
+                <script>
+                    let {goToLog, refresh} =
+                        function () {
+                            const vscode = acquireVsCodeApi();
+                            return {
+                                goToLog: recordPtr => vscode.postMessage({ command: 'goToLog', text: recordPtr }),
+                                refresh: () => vscode.postMessage({ command: 'refresh' })
+                            };
+                        }()
+                </script>
+                <script>
+                    $(document).ready(function () {
+                        var table = $('#resultsTable').DataTable(
+                            {
+                                lengthMenu: [100, 500, 1000],
+                                ordering: true,
+                                order: [],
+                                paging: false,
+                                scrollY: '60vh',
+                                scrollCollapse: true,
+                                orderClasses: false,
+                                columnDefs: [
+                                    { targets: [0], className: "btnCol" },
+                                    { targets: [${getShortColIndexes().join(",")}], className: "shortCol" },
+                                    { targets: [${getSpecialColIndexes("medCol").join(",")}], className: "medCol" },
+                                    { targets: [${getSpecialColIndexes("longCol").join(",")}], className: "longCol" }
+                                ],
+                                createdRow: function (row) {
+                                    $(row).find("td, th").each(function(){
+                                        $(this).attr("title", this.innerText);
+                                     });
+                                }
+                            });
+
+                        $('a.toggle-vis').on( 'click', function (e) {
+                            e.preventDefault();
+                            var column = table.column( $(this).attr('data-column') );
+                            column.visible( ! column.visible() );
+                            $(this).css('text-decoration', column.visible() ? 'none' : 'underline')
+                        } );
+                    });
+                </script>
+                <script>
+                    $('.tableContainer')[0].childNodes.forEach(
+                        childNode => {
+                            if(childNode.textContent.includes(',,')){
+                                childNode.remove();
+                            }
+                        }
+                    );
+                </script>
+            </body>
+        </html>
+        `);
+}
